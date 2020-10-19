499852b72477583599fda7a3683f0449369d3b7c
gamem001 <melgamez@gmail.com>
2020-10-18 21:45:25 -0500

    need to sort

--- static/js/app.js
+++ static/js/app.js
@@ -12,14 +12,13 @@ d3.json('samples.json').then((input) => {
     })
 });
 
-// On change to the DOM, call optionChange()
-d3.selectAll("#selDataset").on("change", optionChanged);
+
 // this is returning data based on dropdown selection
-function optionChanged() {
+function dropDownChange(info) {
     d3.select('#sample-metadata').html("")
-    let dropdownMenu = d3.select("#selDataset");
+    let dropDown= d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
-    let dataset = dropdownMenu.property("value");
+    let dataset = dropDown.property("value");
     // Append data selected from dropdown to demographics table
     let filteredData = data.metadata.filter(input => input['id'] == dataset);
     Object.entries(filteredData[0]).forEach(([key, value]) => {
@@ -28,45 +27,63 @@ function optionChanged() {
             .text(`${key}: ${value}`);
       });
     console.log(filteredData);
-  }
+  };
 
   //need code to reflect a filter on top 10 OTU's when an individual is selcted from the dropdown. 
+function allPlots(info) {
+    d3.json('samples.json').then ((dataPlots) => {
+        console.log(dataPlots);
+        
+        let id = dataPlots.samples[0].otu_ids;
+        let labels = dataPlots.samples[0].otu_labels.slice(0, 10);
+        let sampleValues = dataPlots.samples[0].sample_values.slice(0, 10).reverse();
+      
+        console.log(id);
+        console.log(labels);
+        console.log(sampleValues);
 
-  let id = data.samples.otu_ids;
-  let labels = data.samples.otu_labels;
-  let sampleValues = data.samples.sample_values;
+        //Sort the data by sample Value search results
+        // let topTen = dataPlots.sort((a,b) => b.sampleValues - a.sampleValues);
+        // Slice the first 10 objects for plotting
+        // let slicedData = topTen.slice(0, 10);
+        // console.log(slicedData);
+      
+        //Reverse the array to accommodate Plotly's defaults
+        // let reversedData = slicedData.reverse();
+        
+        let trace1 = {
+            type: 'bar',
+            orientation: 'h',
+            x: sampleValues,
+            y: id,
+            text: labels,
+            name: 'Gross'
+        };
+        
+        // data
+        let dataTrace = [trace1];
+        
+        // Apply the group bar mode to the layout
+        let layout = {
+            title: 'Nastiness Belly Button Bacteria',
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
+        // Render the plot to the div tag with id "bar"
+        Plotly.newPlot('bar', dataTrace,layout);
+    });
 
-  console.log(id);
-  //Sort the data by sample Value search results
-  let otuTen = data.sort( (a,b) => b.sampleValues - a.sampleValues);
-  // Slice the first 10 objects for plotting
-  let slicedData = otuTen.slice(0, 10);
-  console.log(slicedData);
+};
 
-  //Reverse the array to accommodate Plotly's defaults
-  let reversedData = slicedData.reverse();
-  
-  let trace1 = {
-      type: 'bar',
-      orientation: 'h',
-      x: reversedData.map(object => object.sample_values),
-      y: reversedData.map(object => object.sample_values),
-      text: reversedData.map(object => object.sample_values),
-      name: 'Gross'
-  };
-  
-  // data
-  let dataTrace = [trace1];
-  
-  // Apply the group bar mode to the layout
-  let layout = {
-      title: 'Nastiness Belly Button Bacteria',
-      margin: {
-          l: 100,
-          r: 100,
-          t: 100,
-          b: 100
-      }
-  };
-  // Render the plot to the div tag with id "plot"
-  Plotly.newPlot('bar', dataTrace,layout);
+// On change to the DOM, call optionChange()
+//set this to call both the plot change and the dropdown change
+d3.selectAll("#selDataset").on("change", optionChanged);
+function optionChanged(info) {
+    allPlots(info);
+    dropDownChange(info);
+
+};
